ecc286ecde0e23645488c4fafd24a9bd65881dc9
luckhee <dcs1206@naver.com>
2025-06-12 11:10:31 +0900

    Setting ApplicationRunner

--- src/main/java/com/back/global/initData/BaseinitData.java
+++ src/main/java/com/back/global/initData/BaseinitData.java
@@ -1,17 +1,30 @@
 package com.back.global.initData;
 
 
+import com.back.domain.post.entity.Post;
+import com.back.domain.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BaseinitData {
+    @Autowired
+    private PostRepository postRepository;
+
+
+
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
 
         return args -> {
             System.out.println("기본 데이터가 초기화 되었습니다.");
+
+            Post post = new Post();
+            post.setTitle("제목 1");
+
+            postRepository.save(post);
         };
     }
 
